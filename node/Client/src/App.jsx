0b0fe8ccab8b898b324233c9ce0a10d88360a4d4
Adam <hviidadam@gmail.com>
2023-05-08 17:52:07 +0200

    first commit

---
+++ node/Client/src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import "./App.css";
+
+function App() {
+  const [messageHistory, setMessageHistory] = useState([]);
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(
+    "ws://localhost:8000",
+    {
+      onOpen: () => console.log("WebSocket Client Connected"),
+    }
+  );
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const handleClickSendMessage = () => {
+    sendMessage(
+      JSON.stringify({
+        time: new Date().toLocaleString().slice(12, 20),
+        username: username,
+        message: message,
+      })
+    );
+  };
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
+  return (
+    <div>
+      <h2>Choose username</h2>
+      <input type="text" onChange={(e) => setUsername(e.target.value)} />
+      {username != "" ? (
+        <>
+          <h3>The WebSocket is currently {connectionStatus}</h3>
+          <input type="text" onChange={(e) => setMessage(e.target.value)} />
+
+          <p>
+            <button
+              onClick={handleClickSendMessage}
+              disabled={readyState !== ReadyState.OPEN}
+            >
+              Send Message
+            </button>
+          </p>
+          <ul>
+            {messageHistory.map((message, i) => (
+              <p key={i}>{message ? message.data : null}</p>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h3>Username: {username}</h3>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ node/Server/server.js
@@ -0,0 +1,27 @@
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+
+const server = http.createServer();
+const ws = new WebSocketServer({ server });
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
+});
+
+const clients = {};
+
+ws.on('connection', function (connection) {
+  const userId = uuidv4();
+  clients[userId] = connection;
+  console.log(`${userId} connected. Total clients: ${Object.keys(clients).length}`);
+
+  connection.on('message', (message) => {
+    const data = JSON.parse(message);
+    ws.clients.forEach((client) => {
+      client.send(`[${data.time}] ${data.username}: ${data.message}`);
+
+    })
+  });
+});
